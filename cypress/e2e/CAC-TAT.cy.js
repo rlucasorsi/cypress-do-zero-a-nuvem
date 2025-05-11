ff8c116a48411e8652cc9dbd9f32eeee5a5a958c
rlucasorsi <rlucasorsi@gmail.com>
2025-05-11 10:46:46 -0300

    teste Preenchimento campos obrigatorios

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,5 +1,19 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Central de Atendimento ao Cliente TAT", () => {
+  beforeEach(() => {
+    cy.visit("./src/index.html");
+  });
+
+  it("verifica o título da aplicação", () => {
+    cy.title().should("eq", "Central de Atendimento ao Cliente TAT");
+  });
+
+  it("preenche os campos obrigatórios e envia o formulário", () => {
+    cy.get("#firstName").type("Lucas");
+    cy.get("#lastName").type("Orsi");
+    cy.get("#email").type("lucasorsi@teste.com");
+    cy.get("#open-text-area").type("Texto de exemplo na caixa de texto");
+    cy.get('button[type="submit"]').click();
+
+    cy.get(".success").should("be.visible");
+  });
+});
