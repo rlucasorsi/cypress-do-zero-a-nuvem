745b0c1867bc18b64e0e2e04c9df03a8851a7118
rlucasorsi <rlucasorsi@gmail.com>
2025-05-14 06:32:58 -0300

    adding 'select' testing

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -80,4 +80,13 @@ describe("Central de Atendimento ao Cliente TAT", () => {
 
     cy.get(".success").should("be.visible");
   });
+  it("seleciona um produto (YouTube) por seu texto", () => {
+    cy.get("#product").select("YouTube").should("have.value", "youtube");
+  });
+  it("seleciona um produto (Mentoria) por seu valor (value)", () => {
+    cy.get("#product").select("mentoria").should("have.value", "mentoria");
+  });
+  it("seleciona um produto (Blog) por seu índice", () => {
+    cy.get("#product").select(1).should("have.value", "blog");
+  });
 });
